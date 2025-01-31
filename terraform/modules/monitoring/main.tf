ef5b7626b55a3af013de9bf8593af4211fd0b5b3
am2308 <b110020510@gmail.com>
2025-01-31 20:58:25 +0000

    Update main.tf

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
   name = var.aws_cloudwatch_log_group
   retention_in_days = 30
   tags = merge(var.common_tags, {
-    Name        = var.lambda_name
+    Name        = var.aws_cloudwatch_log_group
     Environment = var.environment
   })
 }
