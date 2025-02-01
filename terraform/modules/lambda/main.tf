c334890226674c42f1dd7bcaf5c045b9a491b600
am2308 <b110020510@gmail.com>
2025-02-01 11:54:23 +0000

    feat:added new config

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "zip_lambda" {
     always_run = timestamp() # Ensures this runs on every Terraform apply
   }
   provisioner "local-exec" {
-    command = "cd ${path.module} && zip lambda.zip lambda_function.py && mv lambda.zip ${path.root}/lambda.zip && pwd && ls -lart"
+    command = "cd ${path.module} && zip lambda.zip lambda_function.py && mv lambda.zip ${path.root}/ && pwd && ls -lart"
   }
 }
 
