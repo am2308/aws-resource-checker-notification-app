388e46f52d30e9c858eaffb02689b99e523390f3
am2308 <b110020510@gmail.com>
2025-02-01 12:44:51 +0000

    feat:added new config

--- terraform/modules/lambda/lambda_function.py
+++ terraform/modules/lambda/lambda_function.py
@@ -2,6 +2,9 @@ import boto3
 import os
 import logging
 import json
+import urllib3
+
+http = urllib3.PoolManager()
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -20,11 +23,25 @@ def lambda_handler(event, context):
         for reservation in response["Reservations"]:
             for instance in reservation["Instances"]:
                 if instance["State"]["Name"] == "running":
-                    running_instances.append(instance["InstanceId"])
+                    running_instances.append(instance["InstanceId"] + " with name " + instance["Tags"][0]["Value"])
 
         if running_instances:
             message = f"The following EC2 instances are still running: {', '.join(running_instances)}"
             sns_client.publish(TopicArn=SNS_TOPIC_ARN, Message=message, Subject="Temp EC2 Alert")
+            url = "https://hooks.slack.com/services/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"
+            msg = {
+                "username": "Aws Resource Checker",
+                "text": message,
+            }
+            encoded_msg = json.dumps(msg).encode("utf-8")
+            resp = http.request("POST", url, body=encoded_msg)
+            print(
+                {
+                    "message": msg,
+                    "status_code": resp.status,
+                    "response": resp.data,
+                }
+            )
             logger.info("Notification sent: %s", message)
         else:
             logger.info("No temporary EC2 instances running.")
